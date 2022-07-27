0ba97fc9b2750c9bab5c3e9f18de0b74a13aee66
YaxielGonz <86118100+YaxielGonz@users.noreply.github.com>
2022-07-27 18:12:49 -0400

    Create Main.php

---
+++ src/MiTechMCBE/Crates/Main.php
@@ -0,0 +1,28 @@
+/**
+ * @Autor MiTech MCBE
+ * @copyright 2022 - 2224
+*/
+namespace MiTech\Crates; 
+  
+use pocketmine\plugin\PluginBase; 
+use pocketmine\utils\SingletonTrait; 
+  
+class Main extends PluginBase 
+{ 
+      
+    use SingletonTrait; 
+      
+    public function onLoad(): void 
+    { 
+        self::setInstance($this);  
+    } 
+    public function onEnable(): void 
+    { 
+
+    } 
+  
+    public function onDisable(): void 
+    {
+
+    } 
+} 
