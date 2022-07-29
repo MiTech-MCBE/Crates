bc38b80d85a5d039264b1b336ff707efc4d5d454
YaxielGonz <86118100+YaxielGonz@users.noreply.github.com>
2022-07-29 15:07:32 -0400

    Estructura basica del main

--- src/MiTech/Crates/Main.php
+++ src/MiTech/Crates/Main.php
@@ -1,28 +1,27 @@
+<?php
 /**
  * @Autor MiTech MCBE
  * @copyright 2022 - 2224
 */
-namespace MiTech\Crates; 
-  
-use pocketmine\plugin\PluginBase; 
-use pocketmine\utils\SingletonTrait; 
-  
-class Main extends PluginBase 
-{ 
-      
-    use SingletonTrait; 
-      
-    public function onLoad(): void 
-    { 
-        self::setInstance($this);  
-    } 
-    public function onEnable(): void 
-    { 
+namespace MiTech\Crates;
 
-    } 
-  
-    public function onDisable(): void 
-    {
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
 
-    } 
-} 
+class Main extends PluginBase
+{
+   use SingletonTrait;
+
+   public function onLoad(): void
+   {
+      self::setInstance($this);
+   }
+   public function onEnable(): void
+   {
+
+   }
+   public function onDisable(): void
+   {
+      
+   }
+}
